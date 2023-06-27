e5c1e9d231f0c21c7d6e472d0cee57c12f39731e
purplerising <123110972+purplerising@users.noreply.github.com>
2023-06-27 15:21:45 -0700

    first commit

---
+++ app/api/routes.py
@@ -0,0 +1,133 @@
+from flask import Blueprint, request, jsonify, render_template
+from helpers import token_required
+from models import db, User, Contact, contact_schema, contacts_schema, Book, book_schema, books_schema
+
+api = Blueprint('api',__name__, url_prefix='/api')
+
+@api.route('/contacts', methods = ['POST'])
+@token_required
+def create_contact(current_user_token):
+    first_name = request.json['first_name']
+    last_name = request.json['last_name']
+    email = request.json['email']
+    phone_number = request.json['phone_number']
+    address = request.json['address']
+    user_token = current_user_token.token
+
+    print(f'Library: {current_user_token.token}')
+
+    contact = Contact(first_name, last_name, email, phone_number, address, user_token = user_token )
+
+    db.session.add(contact)
+    db.session.commit()
+
+    response = contact_schema.dump(contact)
+    return jsonify(response)
+
+@api.route('/contacts', methods = ['GET'])
+@token_required
+def get_contacts(current_user_token):
+    a_user = current_user_token.token
+    contacts = Contact.query.filter_by(user_token = a_user).all()
+    response = contacts_schema.dump(contacts)
+    return jsonify(response)
+
+
+@api.route('/contacts/<id>', methods = ['GET'])
+@token_required
+def get_single_contact(current_user_token, id):
+    contact = Contact.query.get(id)
+    response = contact_schema.dump(contact)
+    return jsonify(response)
+
+
+@api.route('/contacts/<id>', methods = ['POST','PUT'])
+@token_required
+def update_contact(current_user_token,id):
+    contact = Contact.query.get(id) 
+    contact.email = request.json['email']
+    contact.first_name = request.json['first_name']
+    contact.last_name = request.json['last_name']
+    contact.phone_number = request.json['phone_number']
+    contact.address = request.json['address']
+    contact.user_token = current_user_token.token
+
+    db.session.commit()
+    response = contact_schema.dump(contact)
+    return jsonify(response)
+
+@api.route('/contacts/<id>', methods = ['DELETE'])
+@token_required
+def delete_contact(current_user_token, id):
+    contact = Contact.query.get(id)
+    db.session.delete(contact)
+    db.session.commit()
+    response = contact_schema.dump(contact)
+    return jsonify(response)
+
+
+@api.route('/books', methods = ['POST'])
+@token_required
+def create_book(current_user_token):
+    isbn = request.json['isbn']
+    year = request.json['year']
+    title = request.json['title']
+    pages = request.json['pages']
+    author = request.json['author']
+    user_id = current_user_token.token 
+    
+    
+    book = Book(isbn, year, title, pages, author, user_id = user_id)
+
+    db.session.add(book)
+    db.session.commit()
+
+    response = book_schema.dump(book)
+    return jsonify(response)
+
+
+
+@api.route('/books', methods = ['GET'])
+@token_required
+def get_books(current_user_token):
+    user_id = current_user_token.token 
+    books = Book.query.filter_by(user_id = user_id).all()
+    response = books_schema.dump(books)
+    return jsonify(response)
+
+
+@api.route('/books/<id>', methods = ['GET'])
+@token_required
+def get_single_book(current_user_token, id):
+    book_Token = current_user_token.token
+    if book_Token == current_user_token.token:
+        book = Book.query.get(id)
+        response = book_schema.dump(book)
+        return jsonify(response)
+    else:
+        return jsonify({'message': "Invalid Token"}), 401
+
+
+@api.route('/books/<id>', methods = ['POST','PUT'])
+@token_required
+def update_book(current_user_token, id):
+    book = Book.query.get(id) 
+    book.isbn = request.json['isbn']
+    book.year = request.json['year']
+    book.title = request.json['title']
+    book.pages = request.json['pages']
+    book.author = request.json['author']
+    book.user_id = current_user_token.token
+
+    db.session.commit()
+    response = book_schema.dump(book)
+    return jsonify(response)
+
+@api.route('/books/<id>', methods = ['DELETE'])
+@token_required
+def delete_book(current_user_token, id):
+    book = Book.query.get(id)
+    db.session.delete(book)
+    db.session.commit()
+    response = book_schema.dump(book)
+    return jsonify(response)
---
+++ app/site/routes.py
@@ -0,0 +1,73 @@
+
+from flask import Blueprint, jsonify, render_template, request, redirect, url_for
+from models import db, User, Book, book_schema, books_schema
+from flask_login import current_user
+
+
+site = Blueprint('site', __name__,template_folder='site_templates')
+
+@site.route('/')
+def home():
+    return render_template('index.html')
+
+@site.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@site.route('/books')
+def books():
+    books = Book.query.all()
+    return render_template('books.html', books=books)
+
+@site.route('/update')
+def update():
+    return render_template('update.html')
+
+@site.route('/create_book', methods=['POST'])
+def create_book():
+    isbn = request.form['isbn']
+    year = request.form['year']
+    title = request.form['title']
+    pages = request.form['pages']
+    author = request.form['author']
+    user_id = current_user.id
+    
+    book = Book(isbn = isbn, year=year, title=title, pages=pages, author=author, user_id = user_id )
+    db.session.add(book)
+    db.session.commit()
+    
+    return redirect(url_for('site.books'))
+
+
+@site.route('/update_book/<id>/edit', methods=['POST', 'PUT'])
+def update_book(id):
+    book = Book.query.filter_by(id=id).first()
+
+    if request.method in ['POST', 'PUT']:
+        print("I printed!")
+        if book:
+            book.isbn = request.form['isbn']
+            book.year = request.form['year']
+            book.title = request.form['title']
+            book.pages = request.form['pages']
+            book.author = request.form['author']
+            book.user_id = current_user.id
+
+            db.session.commit()
+            return redirect(url_for('site.books'))
+        else:
+            return "Book does not exist."
+    return render_template('update.html', book=book)
+
+    
+
+@site.route('/delete_book/<id>', methods=['POST'])
+def delete_book(id):
+    book = Book.query.get(id)
+    if book:
+        db.session.delete(book)
+        db.session.commit()
+        return redirect(url_for('site.books'))
+    
+    
+    return redirect(url_for('site.books'))
